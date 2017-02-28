05ed88bf93ac026f6e49e9be87480c23f4f52365
natasha kelly <kellynatashav@yahoo.com>
2017-02-28 01:26:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog
+  #begin def initialize(name)
+  #  @name = name
+  #end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
+end
