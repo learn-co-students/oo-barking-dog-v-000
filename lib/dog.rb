b2745bd660501d006853b9dcd1ff83e8429dcbba
Emily Sugano <emily.sugano@gmail.com>
2017-03-12 03:08:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+
+  def initialize
+    @bark = puts "woof!"
+  end
+
+  def bark
+    @bark = puts "woof!"
+  end
+
+  def bark
+    @bark
+  end
+    
+end
