b58a58ac0155aec9e828dca3bbd4637dc5b0efd9
Zenia Villa <zeniavilla@hotmail.com>
2017-03-15 19:36:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def initialize
+    name=(name)
+
+    def bark # Define a method for all Dogs
+      puts "woof!"
+    end
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
