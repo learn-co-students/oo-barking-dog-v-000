6fde8e641205cffa263157f8a7d960ad994e3a27
Martyn Conkling <haoleboy3000@yahoo.com>
2017-03-22 16:26:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Dog
+  def name=(new_name)  #this is our name setter method
+    @name = new_name
+  end
+  def name  #this is our name getter methods
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
+end
