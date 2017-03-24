84c20a0791838ac684ac24ae66ec72c71e17f816
Sean Quillen <sean.r.quillen@gmail.com>
2017-03-24 15:14:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(name)  #=> setter
+    @name = name
+  end
+
+  def name        #=> getter
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
