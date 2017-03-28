a1dcd4f4a453013db332f2c429ff773b36158118
jose cobos <luiscobosj@gmail.com>
2017-03-28 21:32:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+##### name #####
+def name=(name)
+  @name = name
+end
+
+def name
+  @name
+end
+################
+
+#### bark #######
+def bark
+  puts "woof!"
+end
+
+################
+end
