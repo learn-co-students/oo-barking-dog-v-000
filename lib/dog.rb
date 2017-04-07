962585ec43ef346d776b5d9608658ac38337a266
Alex Jelly <alexjelly8@gmail.com>
2017-04-07 15:51:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+
+#initialize
+ def initialize
+   @name = name
+ end
+#setter
+  def name=(name)
+    @name = name
+  end
+#getter
+  def name
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
+end
