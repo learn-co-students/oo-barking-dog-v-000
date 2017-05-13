7692e810f59fe4fa52e1c697b3d9ddd5a109357c
Chidinma Eze <ceze1990@gmail.com>
2017-05-13 05:41:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog               #Defines a Dog class
+
+def name=(name)
+    @name = name
+end
+
+def name            # defines a name method
+    @name
+end
+def bark
+  @bark
+    puts "woof!"
+end
+end
