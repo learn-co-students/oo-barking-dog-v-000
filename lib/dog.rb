36cf921106a2cf33ccccaa7bfd77f8b5f96d29cb
Byungwook Yoon <byung90@gmail.com>
2017-05-13 19:37:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  #setters
+  def name=(name)
+    @name = name
+  end
+
+  #getters
+  def name
+    @name
+  end
+
+  #methods
+  def bark
+    puts "woof!"
+  end
+  
+end
