033cb6fd4afb311ace4c20c4d8a557af7f201245
Sheila O'Donnell <sheila@contexttravel.com>
2017-06-15 20:26:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+  def name=(name)
+    @name = name
+    @bark = bark
+
+  end
+
+  def name
+    @name
+  end
+  def bark
+    @bark
+    puts "woof!"
+  end
+end
