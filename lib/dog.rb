2b0f70cbe2809f694867652f4a573cd4b18b030d
Steven Lennis <steven.lennis@icloud.com>
2017-07-01 13:42:49 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+# Class Definition
+class Dog
+# Method Definition
+  def name=(name)
+    @name = name
+  end
+# Method Definition
+  def name
+    @name
+  end
+# Method Definition
+  def bark
+    puts "woof!"
+  end
+end
