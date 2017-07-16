94fc8be19019f6bd23977ce80ae64c87e48ad02e
Isabel Vazquez <12ivazquez@gmail.com>
2017-07-16 20:56:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 # Your code goes here!
 class Dog
+  def name=(name) #writes the name of the dog to an instance variable @name
+    @name = name
+  end
+
+  def name #reads the name of the dog from an instance variable @name
+    @name
+  end
+
   def bark
-    puts "Woof!"
+    puts "woof!"
   end
 end
