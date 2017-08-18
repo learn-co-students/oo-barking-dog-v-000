7d625b212b294e4c2f72a1fb312a9dc516fe7709
Pamela Frieze <pfrieze@gmail.com>
2017-08-18 21:40:22 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog
+  #setter method to give a dog a name equals sign
+  def name=(name)
+@name = name
+  end
+  #getter method to give a dog a name
+  def name
+    @name
+  end
+  #method .bark that puts "woof!" when called on an instance of Dog
+  def bark
+    puts "woof!"
+  end
+end
