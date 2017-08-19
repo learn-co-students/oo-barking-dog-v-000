1c48de7af03faf8fe3b1e17e3b053f5c31048736
Johan Esteves <jsesteves91@gmail.com>
2017-08-19 17:21:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+#attr_accessor :name, :bark
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+
+
+end
