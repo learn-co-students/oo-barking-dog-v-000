e5a5243b26690501d1cbdb369e2e90b2c46d1ba3
Hunter Kleindl <hkleindl@gmail.com>
2017-08-22 21:26:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  # def initialize
+  #   bark
+  # end
+
+  def bark
+    puts "woof!"
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
