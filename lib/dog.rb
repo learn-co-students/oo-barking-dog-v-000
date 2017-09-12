1f75547587b8a752b6b998f94d5c14c7519ff8d3
Tuong Van Nguyen <tvan262@yahoo.com>
2017-09-12 10:03:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog# Your code goes here!
+def name=(fido)
+  @name = fido
+end
+
+def name
+  @name
+end
+end
+  fido = Dog.new
+fido.name = "Fido"
+ puts fido.name
+
+class Dog
+def bark
+    puts "woof!"
+end
+end
+ fido.bark
