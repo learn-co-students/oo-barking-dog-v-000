ea0526349500ae22e02ecc861572591c36709abd
Tyler Pulse <tyler3321@gmail.com>
2017-10-16 22:17:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,19 @@
 class Dog
-  def initialize(name, bark)
-    @name = name
-    @bark = bark
-  end
-
   def name=(name)
     @name = name
-  end
-
-  def bark=(bark)
-    @bark = bark
   end
 
   def name
     @name
   end
 
-  def bark
-    @bark
+  def bark(bark)
+    @bark = bark
   end
+
+  def bark
+    puts "woof!"
+  end
+
+
 end
