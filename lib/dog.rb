bd593a33cd6d4d6e52367f7e33e2f1e76a3d9507
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-05 15:11:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,17 @@
 # Your code goes here!
 class Dog
+
+  #setter method allows you to give a dog a method
   def name= (name)
     @name = name
   end
 
+  #getter method returns an individual dog's name
     def name
       @name
     end
 
+    #puts "woof" when called on an instance of dog
     def bark
       puts "woof!"
     end
