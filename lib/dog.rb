328442b1d69f2d6052ec3b8f53bb8456a8d8e1b8
EPIC448 <samrey2018@gmail.com>
2017-12-07 08:51:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog
+  def initialize
+        @name = name
+        #we leave bark out so that it is not called twice.
+  end
+
+  def name=(dogs_name)
+    @name = dogs_name #this is our setter
+  end
+
+  def name #this is our getter.
+    @name
+  end
+
+
+  def bark
+     puts "woof!"
+  end
+ end
