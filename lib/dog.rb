2d424c36a2e280eda2ff191a3fb0176fa4549f26
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-17 16:55:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Dog
+
+  attr_accessor = :name, :breed
+
+  #def initialize(new_name, new_breed = "Mutt")
+  #  @name = new_name
+  #  @breed = new_breed
+  #end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    return @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
