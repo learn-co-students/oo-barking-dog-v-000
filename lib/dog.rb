af4caa681d6ef5865a351875db07999e1e0e2751
Cam Nguyen <cnguyen1187@gmail.com>
2017-12-20 23:36:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    @fido = (dog_name)
+  end
+
+  def name
+    @fido
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "fido"
