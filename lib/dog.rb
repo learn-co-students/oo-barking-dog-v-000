550edcb515eec7d994efb308a7b573c5aca2c030
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-23 18:27:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog
+
+def initialize()
+  @name = name
+end
+
+def name
+  @name
+end
+
+def name=(value)
+  @name = value
+end
+
+def bark
+  puts "woof!"
+end
+
+
+end
