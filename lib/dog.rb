846693852bd3591201aecda0ac8256859667ca34
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-06 01:42:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Dog
+
+  def initialize (name = "Fido", bark = "is able to bark")
+    @name = name
+    @bark = bark
+  end
+
+  def name=(name = "Fido", bark = "is able to bark")
+    @name = name
+    @bark = bark
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    @bark
+      puts "woof!"
+    end
+
+end
