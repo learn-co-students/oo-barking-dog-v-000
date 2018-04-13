7098c8d1a159de7b3360fb351e51d14247980135
David Moore <hey@iamdavidmoore.com>
2018-04-13 18:43:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Dog
+  #initializes the Dog instance that can be named
+  def name=(name)
+    @name = name
+  end
+
+  #getter methods
+  def name
+    @name
+  end
+
+  #setter method
+  def name=(new_name)
+    @name = new_name
+  end
+
+  #attribute method
+  def bark
+    puts "woof!"
+  end
+end
