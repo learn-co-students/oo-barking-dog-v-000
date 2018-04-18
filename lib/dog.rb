b22b3743ab4b4033c49312d8354aa456d954a15e
Daniel Etter <etter.dt@gmail.com>
2018-04-18 16:36:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog 
+  def bark=(bark)
+    @bark=bark
+  end
+  
+  def bark
+    @bark
+    puts "woof!"
+  end
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    @name
+  end
+end
