4ada77ad83e4eccb64ccfde222f1140028c95d39
Nancy Jo <enciie@gmail.com>
2018-04-20 18:34:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog 
+  
+  def intitialize
+    bark
+  end
+  def bark
+    puts "woof!"
+  end
+
+  def name= (name)
+    @name = name
+  end
+  def name
+    @name
+  end
+    
+end
