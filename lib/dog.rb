da22a9d80a3cff0dd062128554c6d033611eca92
sharonrieger <sharonarieger@gmail.com>
2018-04-30 06:41:08 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Dog
+
+  attr_accessor:name
+
+  def intialize(name)
+    @name= name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.name
+fido.bark
