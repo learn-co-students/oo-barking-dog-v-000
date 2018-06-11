b78da32fe1746e023701f829b8a9629732909910
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-11 18:15:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def intitialize(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_dog_name)
+    @name = new_dog_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
