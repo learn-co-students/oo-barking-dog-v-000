2908c7d08011ba11bec3639e70335df4753c0cc2
Jordan Harris <jharris@wework.com>
2018-06-19 18:31:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Dog
+  def name=(fido)
+    @name = fido
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(beagle)
+    @breed = beagle
+  end
+
+  def breed
+    @breed
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
