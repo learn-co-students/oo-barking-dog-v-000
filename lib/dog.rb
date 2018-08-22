fb9d07b12c2f62de70c4c433f4599a36c4d42c55
Liebe Brand <liebebrand@gmail.com>
2018-08-22 15:15:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Dog
+	def name=(name) # writing the method
+		@name = name
+	end
+
+	def name # reading the method
+		@name
+	end
+
+	def bark
+		puts "woof!"
+	end
+end
