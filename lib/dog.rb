d9440d348b0377d78e356cc3331e2388437b4bbf
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-25 21:40:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+# # Your code goes here!
+# 1. Define a class, called Dog. 
+class Dog
+
+# 2. Write a setter method, `.name=`, that allows you to give a dog a name.
+  def name=(name)
+    @name = name
+  end
+# 3. Write a getter method, `.name` that returns an individual dog's name. 
+  def name
+    @name
+  end
+# 4. Write a method, `.bark`, that `puts` "woof!" when called on an instance of Dog. 
+  def bark
+    puts "woof!"
+  end
+
+end
