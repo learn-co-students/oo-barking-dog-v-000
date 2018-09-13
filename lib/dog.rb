6fa866347ffa067d4fd37ceec2131c936a7b957c
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-13 01:49:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,13 @@
 class Dog
-
   def name
     @name
   end
  
-  def name=(name)
-    @name = name
+  def name=(dog_name)
+    @name = dog_name
   end
   
   def bark 
     puts "woof!"
   end
- 
 end
