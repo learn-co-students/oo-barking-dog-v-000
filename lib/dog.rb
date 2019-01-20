271ee4b2c8f0bb7523334cd6cb016ad25e49eb59
Gigi M <mmgamao1@gmail.com>
2019-01-20 22:56:57 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Dog
+  def bark
+    puts "woof!"
+  end
+  attr_accessor :name 
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+end
+
+
